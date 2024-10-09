b3621a3c704e2505984ab781a9ca8a124e8d8a49
Huỳnh Thiên Bảo <legendrequiem0310@gmail.com>
2024-10-09 12:24:44 +0700

    update template and add problems

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+key = []
+for _ in range(int(input())):   
+    n, k= map(int, input().split())   
+    a = input().split()
+    a = [int(x) for x in a]
+    deck = max(a)
+    card = sum(a)
+    ans = -1
+    while(ans==-1):
+        koin = k
+        while(koin>=0):
+            if((card+koin)%deck!=0):
+                koin-=1
+            else:
+                ans = (card+koin)/deck
+        deck+=1
+        
+    
+    # for i in range(sum(a),k,1):
+    #     if ((sum(a)+i)%n==0):
+    #         ans = max(ans,(sum(a)+i)/n)
+    key.append(ans)
 
-def getMaxPts(a,start):
-    max_val = -1
-    count_val = 0
-    for i in range(start,len(a),2):
-        count_val+=1
-        max_val = max(a[i],max_val)
-    return max_val+count_val
-
-for _ in range(int(input())):       #even faster version
-    n = int(input())
-    a = [int(x) for x in input().split()]
-    even_res = getMaxPts(a,0)
-    odd_res = getMaxPts(a,1)
-    ans = max(even_res,odd_res)
-    print(int(ans))
+print(key)
--- test.py
+++ test.py
@@ -1,5 +1,33 @@
-a = [2.1,2.5,2.7]
-print([int(x) for x in a])
+key = []
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    total_cards = sum(a)  # Total number of cards you have
+    a.sort(reverse=True)  # Sort the card types by how many you have, in descending order
+    
+    max_deck_size = 0
+    
+    # We will iterate over all possible deck sizes from 1 to n
+    for deck_size in range(1, n+1):
+        # Calculate how many full decks of this size we can form
+        full_decks = 0
+        needed_cards = 0  # Cards we need to complete more decks
+        
+        for cards_of_type in a:
+            if cards_of_type >= deck_size:
+                full_decks += cards_of_type // deck_size
+            else:
+                needed_cards += (deck_size - cards_of_type)
+        
+        # If the number of needed cards is less than or equal to the cards we can buy, this deck size is valid
+        if needed_cards <= k:
+            max_deck_size = max(max_deck_size, deck_size)
+    
+    key.append(max_deck_size)
+
+for ans in key:
+    print(ans)
 
 
 
