925e1de291d3e2b76113b35b47f5ea5a825aa027
coldoctopus <legendrequiem0310@gmail.com>
2026-08-25 20:23:32 +0700

    update gitnore

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-key = []
-for _ in range(int(input())):   
-    n, k= map(int, input().split())   
-    a = input().split()
-    a = [int(x) for x in a]
-    deck = max(a)
-    card = sum(a)
-    ans = -1
-    while(ans==-1):
-        koin = k
-        while(koin>=0):
-            if((card+koin)%deck!=0):
-                koin-=1
-            else:
-                ans = (card+koin)/deck
-        deck+=1
-        
+def solve(n, m, records):
+    pts = 0
+    str_check = []
+    intel_check = []
     
-    # for i in range(sum(a),k,1):
-    #     if ((sum(a)+i)%n==0):
-    #         ans = max(ans,(sum(a)+i)/n)
-    key.append(ans)
+    for r in records:
+        if r == 0:
+            pts += 1  
+        elif r > 0:
+            intel_check.append(r)  
+        else:
+            str_check.append(-r)  
+    
+    intel_check.sort()
+    str_check.sort()
+    
+    def pass_check(prefer_str):
+        local_str = 0
+        local_intel = 0
+        local_pts = 0
+        check_passed = 0
+        
+        for r in records:
+            if r == 0:
+                local_pts += 1  
+            elif r > 0:
+                if local_intel >= r:
+                    check_passed += 1  
+                elif local_pts > 0 and not prefer_str:
+                    pts_needed = r - local_intel
+                    if local_pts >= pts_needed:
+                        local_pts -= pts_needed
+                        local_intel = r
+                        check_passed += 1
+            else:
+                if local_str >= -r:
+                    check_passed += 1  
+                elif local_pts > 0 and prefer_str:
+                    pts_needed = -r - local_str
+                    if local_pts >= pts_needed:
+                        local_pts -= pts_needed
+                        local_str = -r
+                        check_passed += 1
+        
+        return check_passed
 
-print(key)
+    result_str_first = pass_check(True)
+    result_intel_first = pass_check(False)
+
+    return max(result_str_first, result_intel_first)
+
+n,m = map(int, input().split())  
+r =  input().split()
+r = [int(x) for x in r]
+
+print(solve(n,m,r))
